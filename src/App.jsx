1f4daaacc3719bee30bc2b1b4640f0aa7fae8983
harpreet singh <happybiostockcode07040@gmail.com>
2023-10-02 21:49:11 +0530

    create  page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
+import Profile from './pages/Profile';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from "./pages/signIn";
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>hello from app side beacuse i am the multi milliniaire</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
 
