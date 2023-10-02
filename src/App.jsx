4de27c8e796f9a108bdc09ce7dba40d87709afda
harpreet singh <happybiostockcode07040@gmail.com>
2023-10-02 19:55:00 +0530

    install react js and tailwind css and creat the first template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-red-500'>hello from app side beacuse i am the multi milliniaire</h1>
+    </div>
+  )
+}
+
+export default App
+
