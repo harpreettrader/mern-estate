930447220131a1d233973ae7f0893d2a52062585
harpreet singh <happybiostockcode07040@gmail.com>
2023-10-04 22:19:41 +0530

    create usermodel schema

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,24 @@
 import mongoose from "mongoose";
-const usershema = mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
+const usershema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    passward:{
-        type:String,
-        required:true,
-       
-    }
-},{timestamps:true}); 
+    passward: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User',usershema)
+const User = mongoose.model("User", usershema);
 
 export default User;
