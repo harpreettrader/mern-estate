30e85a23ab2a590e5f98709413aef5166dbe78ea
harpreet singh <happybiostockcode07040@gmail.com>
2023-10-04 22:15:32 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://happy:happy@cluster0-mern-esate.fxgh1ly.mongodb.net/Clustor0-mern-esate?retryWrites=true&w=majority"
+  );
+}
+main()
+  .then(() => {
+    console.log("CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const usershema = mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    passward:{
+        type:String,
+        required:true,
+       
+    }
+},{timestamps:true}); 
+
+const User = mongoose.model('User',usershema)
+
+export default User;
