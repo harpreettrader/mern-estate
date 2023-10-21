8114f58cc324d5509a4aa22bf718999cf03675f3
harpreet singh <happybiostockcode07040@gmail.com>
2023-10-21 13:58:38 +0530

    update the header and make the profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import Profile from './pages/Profile';
 import About from './pages/About';
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/signIn";
+import PrivateRoute from "./components/privateRoute";
+
+
 
 function App() {
   return (
@@ -14,8 +17,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/about" element={<About />} />
+        <Route  element={<PrivateRoute/>}>
+        <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
